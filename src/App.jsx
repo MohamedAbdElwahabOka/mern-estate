347749b5ac38c62f70af86091584a226e62877fe
MohamedAbdElwahabOka <boka22986@gmail.com>
2024-01-12 22:36:39 +0200

    rout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import {BrowserRouter ,Route,Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import SingUp from './pages/SingUp'
+import SingIn from './pages/SingIn'
+import Profile from './pages/Profile'
+import About from './pages/About'
+
 
 function App() {
   return (
-    <div>
-      <h1 className='text-fuchsia-500'> App</h1>
-    </div>
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/singup" element={<SingUp />} />
+    <Route path="/singin" element={<SingIn />} />
+    <Route path="/profile" element={<Profile />} />
+    <Route path="/about" element={<About />} />
+    </Routes>
+    
+
+    </BrowserRouter>
   )
 }
 
